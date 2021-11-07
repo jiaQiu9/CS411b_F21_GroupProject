e75ca0e46e2c4a9c544374e3f31f16cd9ec125d9
jiaQiu9 <351470636@qq.com>
2021-11-07 15:40:55 -0500

    added a set function for setting book id

--- scr/Book.java
+++ scr/Book.java
@@ -2,7 +2,7 @@
 // The attribute file for the books in the libary
 // each book will have a name, author name, publisher name, and publishing year
 public class Book {
-    private int bookID; // ID indexing for library placement
+    private String bookID; // ID indexing for library placement
     private String title;  // the title of the book
     private String author; // author of the books, for multiple author books
                             // it will just store the authors in one string
@@ -11,7 +11,7 @@ public class Book {
     private boolean available;  // True for in library; False for borrowed
     private ArrayList<BorrowRecord> BorrowRecord; // record every person who borrowed this book
 
-    public Book(int bid, String tit, String auth, String gen, boolean avail){
+    public Book(String bid, String tit, String auth, String gen, boolean avail){
         bookID=bid;
         title=tit;
         author=auth;
@@ -89,6 +89,11 @@ public class Book {
     public void setAvailable(boolean s){
         available=s;
     }
+
+    public void setBookID(String loc, int index){
+        bookID=loc + Integer.toString(index);
+    }
+
 
 
 }

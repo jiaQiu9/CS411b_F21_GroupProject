714523b8fd80ee000471a6b0787f364d4f4b1c85
jiaQiu9 <351470636@qq.com>
2021-11-07 15:58:26 -0500

    added imports to the book file

--- scr/Book.java
+++ scr/Book.java
@@ -1,6 +1,11 @@
 // Authors: CS 411 b F21 Group 2
 // The attribute file for the books in the libary
 // each book will have a name, author name, publisher name, and publishing year
+
+import java.io.*;
+import java.util.*;
+
+
 public class Book {
     private String bookID; // ID indexing for library placement
     private String title;  // the title of the book

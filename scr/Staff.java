d94bcbd282e98a1dd4f43df6cd18d337d39d26ea
jiaQiu9 <351470636@qq.com>
2021-11-07 15:56:35 -0500

    finshed the Staff class, needs commenting

--- scr/Staff.java
+++ scr/Staff.java
@@ -1,12 +1,19 @@
 public Class Staff extends Person{
-    public Staff (String id, int pw, int pnum ){
-        super(id, pnum, pw);
+    protected double salary;
 
+    public Staff (String id, int pw, int pnum, double s  ){
+        super(id, pnum, pw);
+        salary=s;
     }
 
     @Override
     public void printInfo(){
+        super().printInfo();
+        System.out.println("Salary: "+salary+"\n");
+    }
 
+    public double getSalary(){
+        return salary;
         }
 
 

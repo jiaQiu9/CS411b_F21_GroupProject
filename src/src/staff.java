34c127d72e12d33373aecdaf25ea088f6647dbb3
jiaQiu9 <351470636@qq.com>
2021-11-07 23:30:38 -0500

    indentation for staff file

--- src/src/staff.java
+++ src/src/staff.java
@@ -1,18 +1,18 @@
 public class Staff extends Person{
-protected double salary;
+    protected double salary;
 
-public Staff (String id, String pw, int pnum, double s  ){
+    public Staff (String id, String pw, int pnum, double s  ){
         super(id, pnum, pw);
         salary=s;
         }
 
-@Override
-public void printInfo(){
+    @Override
+    public void printInfo(){
         super.printInfo();
         System.out.println("Salary: "+salary+"\n");
         }
 
-public double getSalary(){
+    public double getSalary(){
         return salary;
         }
 

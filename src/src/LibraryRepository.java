3702fd9e62046388dc058e8caae60ccae5637e7e
jiaQiu9 <351470636@qq.com>
2021-11-29 15:46:15 -0500

    Added new class file

--- src/src/LibraryRepository.java
+++ src/src/LibraryRepository.java
@@ -1,23 +1,13 @@
 import com.mysql.cj.jdbc.result.ResultSetMetaData;
 
-import java.io.*;
-import java.sql.*;
-import java.util.*;
-import java.time.*;
-public class Main
-{
-    public static void printF(){
-        String functions="1. Add book\n " +
-                "2. Search book information\n" +
-                "3. Update book information\n" +
-                "4. Add borrower\n" +
-                "5. Add Librarian\n" +
-                "6. Register borrow\n" +
-                "7. Register return\n"+
-                "8. To end the service";
-        System.out.println(functions);
-    }
-    public static void functions(Statement stat,int choice) throws SQLException {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class LibraryRepository {
+    public static void functions(Statement stat, int choice) throws SQLException {
         Scanner input=new Scanner(System.in);
         if(choice==1){
             // Adding or registering a new book fucntionality
@@ -368,7 +358,7 @@ public class Main
 
                     }
 
-                 //END of the IF
+                    //END of the IF
                 }
                 else if (uCBid==0){
                     // when the user knows the borrower id
@@ -481,54 +471,4 @@ public class Main
         }
     }
 
-    public static void main(String[] args){
-
-        Scanner input=new Scanner(System.in);
-        System.out.println("You need to connect to a mysql database, before using the service");
-        System.out.println("");
-
-        /*System.out.println("Enter database URL: ");
-        String dbURL=input.next();
-
-        System.out.println("Enter database username: ");
-        String dbusername=input.next();
-
-        System.out.println("Enter database password");
-        String dbpasw=input.next();
-
-        System.out.println("database URL"+dbURL +" "+"database username: "+dbusername+" "+"database password"+""+dbpasw);
-        */
-        try {
-            // Connecting to mysql server
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/librarydb","root","09062000");
-
-            Statement statement =connection.createStatement();
-            //LibraryRepository libraryRepository=new LibraryRepository();
-            // starting the service of this software/ program
-            while(true){
-                // the while loop that allows the user to continuely apply functions, until they choose to end
-                // the service
-                printF();
-                System.out.print("\nPlease select the service that you want to conduct.:");
-                int choice=input.nextInt();
-                System.out.println("\n");
-                if(choice==8){
-                    System.out.println("Now the service will be shutting off");
-                    break;
-                }
-                else if (choice==1 || choice ==2|| choice==3|| choice==4 || choice ==5|| choice==6 || choice ==7){
-                    System.out.println("For the library system that interact with the database.");
-                    LibraryRepository.functions(statement,choice); // calling the functions and applying the user's choice
-                }
-                System.out.println("Please make another choice\n\n");
-            }
-            System.out.println("System shutting down.");
-
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-
-
-    }
 }
